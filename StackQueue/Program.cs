2cdb67602283f073825e765eead881597f3ad99e
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-09-24 11:22:19 -0700

    adding enqueue and display

--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -6,15 +6,21 @@ namespace StackQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stack and Queue");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            linkedListStack.Display();
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            linkedListStack.IsEmpty();
+            /*  Console.WriteLine("Welcome to Stack and Queue");
+              LinkedListStack linkedListStack = new LinkedListStack();
+              linkedListStack.Push(70);
+              linkedListStack.Push(30);
+              linkedListStack.Push(56);
+              linkedListStack.Display();
+              linkedListStack.Peek();
+              linkedListStack.Pop();
+              linkedListStack.IsEmpty();
+            */
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Display();
         }
     }
 }
