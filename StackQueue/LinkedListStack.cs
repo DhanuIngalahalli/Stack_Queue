4434ac31045490258ed52bc80e4d950ad7d301bb
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-09-24 11:08:06 -0700

    adding push and disply

---
+++ StackQueue/LinkedListStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueue
+{
+    class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        /// <summary>
+        /// create push method
+        /// </summary>
+        /// <param name="value"></param>
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        /// <summary>
+        /// create display method
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -7,6 +7,11 @@ namespace StackQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
         }
     }
 }
